1335d2fe52b48700d5238a50930dc3807df7a66a
Hans Friedrich <hans.friedrich@me.com>
2021-12-16 21:53:24 +0100

    switched to ws client

--- src/model/fellerwiserclient.ts
+++ src/model/fellerwiserclient.ts
@@ -4,15 +4,15 @@ import fetch from 'node-fetch';
 import { JSendResponse } from './JSendResponse';
 import { Load } from './load';
 import { LoadState } from './loadstate';
-import WebSocket = require('websocket');
 import { EventEmitter } from 'stream';
+import WebSocket from 'ws';
 
 
 export class FellerWiserClient{
   private authkey: string;
   private authToken : string | undefined;
   private log : Logger;
-  private websocket : WebSocket.client;
+  private websocket : WebSocket;
   private baseUrl: string;
   private loadstates : Map<number, LoadState>;
   public loadStateChange : EventEmitter;
@@ -21,50 +21,36 @@ export class FellerWiserClient{
     this.log = log;
     this.authkey = config.authkey;
     this.log.debug('feller client built');
-    this.websocket = new WebSocket.client();
+    this.websocket = new WebSocket('ws://' + config.ip + '/api', [], {headers: {'Authorization': 'Bearer ' + config.authkey}} );
     this.baseUrl = 'http://' + config.ip + '/api';
     this.loadstates = new Map<number, LoadState>();
     this.loadStateChange = new EventEmitter();
 
-    this.websocket.on('connectFailed', (error) => {
-      this.log.error('websocket connection failed with', error);
+    this.websocket.on('close', () => {
+      this.log.debug('websocket connection closed');
     });
 
-    this.websocket.on('connect', (connection) => {
-      // when the websocket connection is established read the load states
-      this.log.debug('websocket connection established');
-      this.log.debug('requesting dump_load');
-      connection.send(JSON.stringify({'command': 'dump_loads'}));
-
-      connection.on('error', (error) => {
-        this.log.error('connection error', error.toString());
-      });
-
-      connection.on('close', () => {
-        this.log.debug('websocket connection closed');
-      });
-
-      connection.on('message', (message) => {
-        // when a new message is received, add the loadstate to the cache
-        if (message.type === 'utf8'){
-          const jsonMessage = JSON.parse(message.utf8Data);
-          const id = jsonMessage.load.id as number;
-          const loadstate = jsonMessage.load.state as LoadState;
-
-          // write the received state to the state cache
-          this.loadstates.set(id, loadstate);
-          this.log.debug('new loadstate for ', id, 'to', loadstate);
-
-          // inform the listeners for this load
-          this.loadStateChange.emit(id.toString(), loadstate);
-
-        }
-
-      });
+    this.websocket.on('error', (error) => {
+      this.log.error('connection error', error.toString());
     });
 
-    // finally establish the connection to the websocket
-    this.websocket.connect('ws://' + config.ip + '/api', '', undefined, {'Authorization': 'Bearer ' + config.authkey} );
+    this.websocket.on('open', () =>{
+      this.websocket.send(JSON.stringify({'command': 'dump_loads'}));
+
+    });
+
+
+    this.websocket.on('message', (message) => {
+      const jsonMessage = JSON.parse(message.toLocaleString());
+      const id = jsonMessage.load.id as number;
+      const loadstate = jsonMessage.load.state as LoadState;
+
+      // write the received state to the state cache
+      this.loadstates.set(id, loadstate);
+
+      // inform the listeners for this load
+      this.loadStateChange.emit(id.toString(), loadstate);
+    });
   }
 
   async getLoads() : Promise<Load[]>{
